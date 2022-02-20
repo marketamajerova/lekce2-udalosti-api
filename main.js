d4c0a80487e2700951f9c6531f023fe676132d5e
Marketa Majerova <marketamajerova.6@gmail.com>
2022-02-20 10:42:45 +0100

    Adding assignment number 3

--- main.js
+++ main.js
@@ -14,8 +14,36 @@ function zbarviNaCerveno() {
 function zvetsiPoKliknuti() {
     /* Casti funkce vypujceny ze stack overflow. Moje pokusy nefungovaly. 
     Jednotlive funkce jsem si vsak vyhledala v dokumentaci a mam poci, ze je dostatecne chapu. */
-    
+
     style = window.getComputedStyle(odstavec).getPropertyValue('font-size');
     currentSize = parseFloat(style);
     odstavec.style.fontSize = (currentSize + 1) + 'px';
 }
+
+let audioFile = document.getElementById("zvukovaStopa");
+
+function startAudio() {
+    audioFile.play();
+}
+
+function pauseAudio() {
+    audioFile.pause();
+}
+
+function quitDownAudio() {
+    audioFile.volume = 0.1;
+}
+
+function middleVolume() {
+    audioFile.volume = 0.5;
+}
+
+function maxVolume() {
+    audioFile.volume = 1;
+}
+
+function restartAudio() {
+    audioFile.load();
+    audioFile.play();
+}
+
