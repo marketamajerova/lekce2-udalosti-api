90c1cf1a2769605523d09745e77575643c386635
Marketa Majerova <marketamajerova.6@gmail.com>
2022-02-20 10:06:32 +0100

    Adding assignment number 2

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// tady je místo pro náš program
-
 let nadpis = document.querySelector('h1');
 nadpis.classList.add('zeleny');
 nadpis.classList.remove('zeleny');
@@ -7,7 +5,17 @@ nadpis.classList.remove('zeleny');
 let cverecek = document.querySelector('div.zluty');
 cverecek.classList.add('ctverecek');
 
-function zbarviNaCerveno(){
-    odstavec = document.getElementById("lev");
+let odstavec = document.getElementById("lev");
+
+function zbarviNaCerveno() {
     odstavec.classList.toggle("cerveny");
+}
+
+function zvetsiPoKliknuti() {
+    /* Casti funkce vypujceny ze stack overflow. Moje pokusy nefungovaly. 
+    Jednotlive funkce jsem si vsak vyhledala v dokumentaci a mam poci, ze je dostatecne chapu. */
+    
+    style = window.getComputedStyle(odstavec).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    odstavec.style.fontSize = (currentSize + 1) + 'px';
 }
